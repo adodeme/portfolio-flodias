b62d42f0f10b6885b6644688cffa8df9f9cf5793
adodeme <adodemeflodias@gmail.com>
2026-06-10 09:16:24 +0100

    Portfolio final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,20 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-lg bg-slate-950/60 border-b border-slate-800">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
 
-        <h1 className="font-bold text-2xl bg-gradient-to-r from-cyan-400 to-violet-500 bg-clip-text text-transparent">
-          Flodias.dev
-        </h1>
+        <div className="flex items-center gap-3">
+
+
+            <img
+                src="/mylogo.png"
+                alt="Logo"
+                className="w-10 h-10 object-contain"
+            />
+
+            <h1 className="font-bold text-2xl bg-gradient-to-r from-cyan-400 to-violet-500 bg-clip-text text-transparent">
+                Flodias.dev
+            </h1>
+
+        </div>
 
         <ul className="hidden md:flex gap-8">
           <li><a href="#about">À propos</a></li>
